eab886a36b8e7e972772f174de2b837e7d999413
ISpEedTEST <126049899+ISpEedTEST@users.noreply.github.com>
2026-04-25 21:18:54 +0300

    Change app listener to client listener in index.js

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ for (const file of eventFiles) {
 }
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, "0.0.0.0", () => {
+client.listen(PORT, "0.0.0.0", () => {
     console.log(`✅ Server is running on port ${PORT}`);
 });
 client.login(process.env.TOKEN);
